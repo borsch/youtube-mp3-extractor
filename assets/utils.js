22110bdd4e716bf7200a75b45e731fde6fd9dd63
Oleh Kurpiak <oleh.kurpiak@gmail.com>
2018-03-01 00:36:29 +0200

    video name for separate video

--- assets/utils.js
+++ assets/utils.js
@@ -17,3 +17,24 @@ function get_url_params() {
 function generate_iframe(video_id) {
     return '<iframe class="button-api-frame" src="https://youtubemp3api.com/@api/button/mp3/' + video_id + '" allowtransparency="true" scrolling="no"></iframe>'
 }
+
+function video_block(title, video_id) {
+    var $block = $( '<div>' +
+                        '<div>' +
+                            '<p class="left">' +
+                                title +
+                            '</p>' +
+                            '<p class="right">' +
+                                '<button class="load-button" data-video="' + video_id + '">Load</button>' +
+                            '</p>' +
+                        '</div>' +
+                        '<div class="clear"></div>' +
+                        '<div class="iframe"></div>' +
+                    '</div>');
+
+    $block.find('.load-button').click(function(){
+        $block.find('.iframe').html(generate_iframe($(this).data('video')));
+    });
+
+    return $block;
+}
